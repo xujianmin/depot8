0b1037335b61885c94b133cdb8800beec42d8e30
DESKTOP-HOME\iamji <jianmin.xu@usingnow.cn>
2025-03-02 01:26:57 +0800

    Iteration A1, A2.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :set_active_storage_url_options
+
+  private
+
+  def set_active_storage_url_options
+    ActiveStorage::Current.url_options = { host: request.base_url }
+  end
 end
